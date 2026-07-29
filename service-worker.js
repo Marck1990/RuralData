2e204b82c9c91c5d97530ab6041439b4956fc1f2
Marcos Juarez <pirotec50@gmail.com>
2026-07-28 22:05:28 -0300

    Corrige cache de PWA y actualiza service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ruraldata-cache-v6";
+const CACHE_NAME = "ruraldata-cache-v7";
 
 const ARCHIVOS_CACHE = [
   "index.html",
@@ -24,11 +24,29 @@ const ARCHIVOS_CACHE = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(ARCHIVOS_CACHE);
     })
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(claves => {
+      return Promise.all(
+        claves.map(clave => {
+          if (clave !== CACHE_NAME) {
+            return caches.delete(clave);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
